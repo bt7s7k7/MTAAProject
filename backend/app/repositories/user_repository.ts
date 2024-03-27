035918026ae0358a1e53720ad984a7e3fe3494d8
Branislav Trstenský <bt7s7k7@hotmail.sk>
2024-03-27 14:09:54 +0100

    Serving user icons

--- backend/app/repositories/user_repository.ts
+++ backend/app/repositories/user_repository.ts
@@ -1,5 +1,7 @@
 import Invite from '#models/invite'
 import User from '#models/user'
+import app from '@adonisjs/core/services/app'
+import { mkdir } from 'node:fs/promises'
 
 export class UserRepository {
   async isFriendsWith(user: User, friend: User) {
@@ -9,7 +11,20 @@ export class UserRepository {
 
   async searchUsers(query: string) {
     const limit = 10
-    return await User.query().whereILike('full_name', query).limit(limit)
+    return await User.query().whereILike('fullName', `%${query}%`).limit(limit)
+  }
+
+  protected _iconsPath: string | null = null
+  async getIconsPath() {
+    if (this._iconsPath === null) {
+      this._iconsPath = app.makePath('uploads', 'icons')
+      await mkdir(this._iconsPath, { recursive: true }).catch((error) => {
+        if (error.code === 'EEXIST') return null
+        throw error
+      })
+    }
+
+    return this._iconsPath
   }
 
   async sendInvite(from: User, to: User) {
--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -7,7 +7,9 @@
 |
 */
 
+import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
+import { normalize, sep } from 'node:path'
 import { middleware } from './kernel.js'
 const FriendsController = () => import('#controllers/friends_controller')
 const UserController = () => import('#controllers/users_controller')
@@ -17,6 +19,25 @@ router.get('/', async () => {
   return {
     hello: 'world',
   }
+})
+
+const PATH_TRAVERSAL_REGEX = /(?:^|[\\/])\.\.(?:[\\/]|$)/
+
+router.get('/uploads/icons/default', ({ response }) => {
+  return response.download(app.makePath('default_icon.png'))
+})
+
+router.get('/uploads/*', ({ request, response }) => {
+  const filePath = request.param('*').join(sep)
+  const normalizedPath = normalize(filePath)
+
+  if (PATH_TRAVERSAL_REGEX.test(normalizedPath)) {
+    return response.badRequest('Malformed path')
+  }
+
+  const absolutePath = app.makePath('uploads', normalizedPath)
+  console.log(absolutePath)
+  return response.download(absolutePath)
 })
 
 router
