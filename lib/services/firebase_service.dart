c37d66830037e694d258201e36f8b3075173f623
Matus <matokolejak@gmail.com>
2024-04-21 21:04:57 +0200

    Crashlytics

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,5 +1,7 @@
 // firebase_service.dart
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart'; // Required for kReleaseMode
 import 'package:mtaa_project/app/debug_page.dart';
 import 'package:mtaa_project/constants.dart';
 
@@ -13,5 +15,15 @@ class FirebaseService {
       options: firebaseOptions,
     );
     debugMessage("[Firebase] Firebase app loaded");
+
+    if (kReleaseMode) {
+      Function originalOnError = FlutterError.onError ?? (FlutterErrorDetails details) {};
+      FlutterError.onError = (FlutterErrorDetails details) async {
+        originalOnError(details);
+        await FirebaseCrashlytics.instance.recordFlutterError(details);
+      };
+    }
+    
+    debugMessage("[Firebase] Crashlytics initialized");
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import firebase_analytics
 import firebase_core
+import firebase_crashlytics
 import firebase_messaging
 import geolocator_apple
 import path_provider_foundation
@@ -15,6 +16,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
